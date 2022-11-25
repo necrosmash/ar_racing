8bd7a50599a7abc9805df93a1391c5788455f464
hedy-99 <hedypet@hotmail.com>
2022-11-25 16:47:24 +0100

    Trying to make the car fly, added a ramp?

--- Assets/Scripts/SimpleCarController.cs
+++ Assets/Scripts/SimpleCarController.cs
@@ -17,6 +17,24 @@ public class SimpleCarController : MonoBehaviour
     public float maxMotorTorque;
     public float maxSteeringAngle;
 
+    public bool grounded = false;
+    public float groundedRaycastDistance = 0.5f;
+
+    public float gravityMultiplier = -0.8f;
+
+    public bool IsGrounded()
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (!Physics.Raycast(axleInfo.leftWheel.transform.position, -Vector3.up, groundedRaycastDistance))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     // finds the corresponding visual wheel
     // correctly applies the transform
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
@@ -43,6 +61,8 @@ public class SimpleCarController : MonoBehaviour
         float motor = maxMotorTorque * Input.GetAxis("Vertical");
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
+        grounded = IsGrounded();
+        
         foreach (AxleInfo axleInfo in axleInfos)
         {
             if (axleInfo.steering)
@@ -58,5 +78,18 @@ public class SimpleCarController : MonoBehaviour
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
+
+        if (!grounded)
+        {
+            // Apply gravity multiplier to rigidbody
+            //GetComponent<Rigidbody>().AddForce(Physics.gravity * gravityMultiplier, ForceMode.Acceleration);
+
+            //GetComponent<Rigidbody>().AddForce(transform.forward * motor, ForceMode.Acceleration);
+
+            // steer left and right
+            //GetComponent<Rigidbody>().AddTorque(transform.up * steering, ForceMode.Acceleration);
+            //GetComponent<Rigidbody>().useGravity = false;
+        }
+
     }
 }
