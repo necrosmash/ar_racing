eaa611c7e40632c4875b930907e76a351783fe28
hedy-99 <hedypet@hotmail.com>
2023-01-14 17:11:17 +0100

    images for tracking added

---
+++ Assets/Scripts/ImageDetecting.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+public class ImageDetecting : MonoBehaviour
+{
+    ARTrackedImageManager imgtracker;
+
+    public Color carColor = Color.white;
+    private MeshRenderer mesh;
+
+    void Awake()
+    {
+        imgtracker = GetComponent<ARTrackedImageManager>();
+    }
+    void OnEnable()
+    {
+        imgtracker.trackedImagesChanged += myEventHandler;
+    }
+    void OnDisable()
+    {
+        imgtracker.trackedImagesChanged -= myEventHandler;
+    }
+    void myEventHandler(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+
+        foreach (ARTrackedImage img in eventArgs.added)
+        {
+            handleTracking(img);
+        }
+        foreach (ARTrackedImage img in eventArgs.updated)
+        {
+            handleTracking(img);
+        }
+    }
+    void handleTracking(ARTrackedImage img)
+    {
+        string key;
+        if (img.trackingState == TrackingState.None)
+        {
+            return;
+        }
+
+        key = img.referenceImage.name;
+
+        Color car1 = Color.yellow;
+        Color car2 = Color.red;
+        Color car3 = Color.blue;
+        Color car4 = Color.magenta;
+        Color car5 = Color.green;
+
+
+        carColor = car1;
+        
+        foreach (Transform child in transform)
+        {
+            // get the mesh renderer of child
+            mesh = child.GetComponent<MeshRenderer>();
+            // if child has a mesh renderer
+            if (mesh != null)
+            {
+                // render the colour to the cat
+                mesh.material.color = carColor;
+            }
+        }
+        // get the childern of the child called "wings"
+        foreach (Transform child in transform.Find("wings"))
+        {
+            // get the mesh renderer of child
+            mesh = child.GetComponent<MeshRenderer>();
+            // if child has a mesh renderer
+            if (mesh != null)
+            {
+                // render the colour to the cat
+                mesh.material.color = carColor;
+            }
+        }
+
+        
+
+        Debug.Log("Found an image: " + img.referenceImage.name + " ("
+           + img.trackingState + ")");
+    }
+
+    private void Update()
+    {
+        if(mesh == null)
+        {
+            mesh = GameObject.Find("Car(Clone)").transform.GetComponent<MeshRenderer>();
+        }
+    }
+}
+
