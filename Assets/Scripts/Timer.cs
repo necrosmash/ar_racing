825da1bcd99d8f669dd3408eb8fccab16d9921e5
hedy-99 <hedypet@hotmail.com>
2023-01-14 15:53:10 +0100

    display timer corrected

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -27,47 +27,61 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(car.currentCheckpoint);
-        if (car.currentCheckpoint == start)
+        if(car == null)
         {
-            startedRace = true;
+            car = GameObject.Find("Car(Clone)").GetComponent<SimpleCarController>();
+            
+        }
+        if (car == null)
+        {
+            return;
+        }
 
-            if (fullLap)
+        {
+            Debug.Log(car.currentCheckpoint);
+            if (car.currentCheckpoint == start)
             {
-                fullLap = false;
-                lapFinished = true;
-            }
-        }
+                startedRace = true;
 
-        if (startedRace)
-        {
-            timer += Time.deltaTime;
-            lapTime += Time.deltaTime;
-        }
-        
-        if (car.currentCheckpoint.NextCheckpoint == start)
-        {
-            fullLap = true;
-        }
-
-        if (lapFinished)
-        {
-            if (lapTime < bestTime)
-            {
-                bestTime = lapTime;
-                lapTime = 0f;
+                if (fullLap)
+                {
+                    fullLap = false;
+                    lapFinished = true;
+                }
             }
 
-            lapFinished = false;
+            if (startedRace)
+            {
+                timer += Time.deltaTime;
+                lapTime += Time.deltaTime;
+            }
+
+            if (car.currentCheckpoint.NextCheckpoint == start)
+            {
+                fullLap = true;
+            }
+
+            if (lapFinished)
+            {
+                if (lapTime < bestTime)
+                {
+                    bestTime = lapTime;
+                    lapTime = 0f;
+                }
+
+                lapFinished = false;
+            }
+            
         }
 
         OnGUI();
+
     }
 
     private void Start()
     {
-        start = GameObject.Find("Track").transform.Find("CheckPointHolder").transform.Find("CheckPoint 0(Clone)").GetComponent<CheckPointSingle>();
-        car = GameObject.Find("Car").GetComponent<SimpleCarController>();
+        start = GameObject.Find("Track").transform.Find("CheckPointHolder").transform.Find("CheckPoint 1(Clone)").GetComponent<CheckPointSingle>();
+        
     }
     private void Awake()
     {
@@ -76,7 +90,7 @@ public class Timer : MonoBehaviour
     void OnGUI()
     {
         text.text = Mathf.Floor(timer / 60).ToString("00") + ":" + Mathf.FloorToInt(timer % 60).ToString("00");
-        bestText.text = Mathf.Floor(bestTime/ 60).ToString("00") + ":" + Mathf.FloorToInt(bestTime % 60).ToString("00");
+        bestText.text = "Best lap time " + Mathf.Floor(bestTime/ 60).ToString("00") + ":" + Mathf.FloorToInt(bestTime % 60).ToString("00");
 
     }
 }
